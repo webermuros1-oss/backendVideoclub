7e214ba37edb6089865299d1264a7753bdbd5c64
webermuros1 <webermuros1@gmail.com>
2026-03-23 08:56:46 +0100

    implement on main movieView.showMovies();

--- src/main/java/org/factoria/Main.java
+++ src/main/java/org/factoria/Main.java
@@ -4,6 +4,7 @@ import org.factoria.controller.MovieController;
 import org.factoria.repository.MovieRepositoryImpl;
 import org.factoria.view.MovieView;
 
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,5 +13,6 @@ public class Main {
         MovieView movieView = new MovieView(movieController);
 
         movieView.generateMovie();
+        movieView.showMovies();
     }
 }
